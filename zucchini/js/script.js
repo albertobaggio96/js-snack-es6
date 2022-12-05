922df1be7ad4985c46aa9b6cb46637ea4337eb22
albertobaggio96 <albertobaggio96@gmail.com>
2022-12-05 12:39:40 +0100

    zucchini

---
+++ zucchini/js/script.js
@@ -0,0 +1,58 @@
+/**
+ *
+ * Crea 10 oggetti che rappresentano una zucchina, 
+ * indicandone per ognuno varietà, peso e lunghezza.
+ *
+ * Calcola quanto pesano tutte le zucchine.
+ *
+ */
+
+const zucchineList =[
+  {varietà: "gialla",
+  peso: "1",
+  lunghezza: "12"},
+
+  {varietà: "gialla",
+  peso: "1.3",
+  lunghezza: "12"},
+
+  {varietà: "gialla",
+  peso: "1",
+  lunghezza: "12"},
+
+  {varietà: "gialla",
+  peso: "1.3",
+  lunghezza: "12"},
+
+  {varietà: "gialla",
+  peso: "1.2",
+  lunghezza: "12"},
+
+  {varietà: "gialla",
+  peso: "1.3",
+  lunghezza: "12"},
+
+  {varietà: "gialla",
+  peso: "1",
+  lunghezza: "12"},
+
+  {varietà: "gialla",
+  peso: "1.3",
+  lunghezza: "12"},
+
+  {varietà: "gialla",
+  peso: "1.2",
+  lunghezza: "12"},
+
+  {varietà: "gialla",
+  peso: "3",
+  lunghezza: "12"},
+]
+
+let sum = 0;
+
+for(let i= 0; i < zucchineList.length; i++){
+  // console.log(zucchineList[i].peso);
+  sum += parseInt(zucchineList[i].peso)
+}
+console.log(sum)
